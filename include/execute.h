fddcc5103303473783a5e3a92dbec9dd31aa2c64
Zephyroku <format.e.drive@gmail.com>
2026-08-16 13:32:24 +0200

    Add new instruction handling functions to execute.h

--- include/execute.h
+++ include/execute.h
@@ -3,16 +3,23 @@
 #include "types.h"
 
 void execute_instruction(struct CPU_State* cpu, opcode op);
-
-// --- Phase 3: MOV / ADD / SUB ---
+void handle_and_reg(struct CPU_State* cpu, byte reg_index);
+void handle_or_reg(struct CPU_State* cpu, byte reg_index);
+void handle_xor_reg(struct CPU_State* cpu, byte reg_index);
+void handle_shl(struct CPU_State* cpu);
+void handle_shr(struct CPU_State* cpu);
+void handle_rol(struct CPU_State* cpu);
 void handle_mov_imm(struct CPU_State* cpu, byte reg_index, byte value);
 void handle_mov_reg(struct CPU_State* cpu, byte dest_reg, byte src_reg);
 void handle_add_imm(struct CPU_State* cpu, byte immediate);
 void handle_add_reg(struct CPU_State* cpu, byte reg_index);
 void handle_sub_imm(struct CPU_State* cpu, byte immediate);
 void handle_sub_reg(struct CPU_State* cpu, byte reg_index);
-
-// --- Phase 4: Jumps / CALL / RET ---
+void handle_cmp_reg(struct CPU_State* cpu, byte reg_index);
+void handle_cmp_imm(struct CPU_State* cpu, byte immediate);
+void handle_test_reg(struct CPU_State* cpu, byte reg_index);
+void handle_inc_reg(struct CPU_State* cpu, byte reg_index);
+void handle_dec_reg(struct CPU_State* cpu, byte reg_index);
 void handle_jmp(struct CPU_State* cpu, address target);
 void handle_jz(struct CPU_State* cpu, address target);
 void handle_jnz(struct CPU_State* cpu, address target);
@@ -22,5 +29,7 @@ void handle_js(struct CPU_State* cpu, address target);
 void handle_jns(struct CPU_State* cpu, address target);
 void handle_call(struct CPU_State* cpu, address target);
 void handle_ret(struct CPU_State* cpu);
+void handle_mov_mem_to_reg(struct CPU_State* cpu, byte reg_index, address addr);
+void handle_mov_reg_to_mem(struct CPU_State* cpu, address addr, byte reg_index);
 
 #endif // EXECUTE_H
